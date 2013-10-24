d627b1393246b6ee585c7da21bc445f65b2e71a4
saddy-ann <saddy666@gmail.com>
2013-10-24 18:21:38 +0300

    - sqlite

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131019140052) do
+ActiveRecord::Schema.define(version: 20131023123702) do
 
   create_table "projects", force: true do |t|
     t.string   "name"
@@ -31,5 +31,12 @@ ActiveRecord::Schema.define(version: 20131019140052) do
   end
 
   add_index "tasks", ["project_id"], name: "index_tasks_on_project_id"
+
+  create_table "users", force: true do |t|
+    t.string   "email"
+    t.string   "password"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
